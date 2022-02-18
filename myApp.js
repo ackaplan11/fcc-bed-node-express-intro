a78f38fcb87df1697631e1334983d2ee5e9584cf
Andrew Kaplan <ac.kaplan11@berkeley.edu>
2022-02-18 14:11:17 -0800

    added /now route which returns json object with the current time

--- myApp.js
+++ myApp.js
@@ -3,14 +3,28 @@ require('dotenv').config()
 var express = require('express');
 var app = express();
 
+console.log(new Date().toString())
+
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.path} - ${req.ip}`)
     next()
 })
 
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString()
+    next() 
+}, (req, res) => {
+    
+    res.json( { time : req.time } )
+})
+
+//REST API - REpresentational State Transfer API allows data exchanges without the need for clients to know
+//detials about the server, only the endpoint and the verb
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 })
+
 
 app.get('/json', (req, res) => {
     const message = (process.env.MESSAGE_STYLE == 'uppercase') ? "HELLO JSON" : "Hello json"
@@ -21,9 +35,8 @@ app.get('/json', (req, res) => {
 //Serves 'public' assets to /public route
 //QUESTION - why if you need to mount middlewhere before there request, does the this middleware
 //serve public stylesheets to the html file even though it is implemented before the app.get
+
 app.use('/public', express.static('public'))
 
-//REST API - REpresentational State Transfer API allows data exchanges without the need for clients to know
-//detials about the server, only the endpoint and the verb
-
+//QUESTION - what is the imporance of module.exports = app
 module.exports = app;
