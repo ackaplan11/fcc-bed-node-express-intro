1d519b75e430e8b29766ce8843eba79e017f6316
Andrew Kaplan <ac.kaplan11@berkeley.edu>
2022-02-17 22:07:01 -0800

    added basic loggin middelware to root requests

--- myApp.js
+++ myApp.js
@@ -2,7 +2,11 @@ require('dotenv').config()
 
 var express = require('express');
 var app = express();
-console.log('Hello World')
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`)
+    next()
+})
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
@@ -12,7 +16,11 @@ app.get('/json', (req, res) => {
     const message = (process.env.MESSAGE_STYLE == 'uppercase') ? "HELLO JSON" : "Hello json"
     res.json({"message": message})
 })
+
+
 //Serves 'public' assets to /public route
+//QUESTION - why if you need to mount middlewhere before there request, does the this middleware
+//serve public stylesheets to the html file even though it is implemented before the app.get
 app.use('/public', express.static('public'))
 
 //REST API - REpresentational State Transfer API allows data exchanges without the need for clients to know
